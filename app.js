fbe50537a45f032b404875c59f945244381f2152
tahed1617 <101617512+tahed1617@users.noreply.github.com>
2022-07-07 23:40:35 -0500

    corrigiendo ruta y agregando .env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const healthRoutes = require('./routes/health');
 
-app.use('/api/health/status', healthRoutes);
+app.use('/api/health/', healthRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
