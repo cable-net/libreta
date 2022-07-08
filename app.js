578bb4b40908ca4b7e2f9784b726a323b6e8dd27
tahed1617 <101617512+tahed1617@users.noreply.github.com>
2022-07-07 21:58:23 -0500

    agregando ruta al health

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const healthRoutes = require('./routes/health');
+
+app.use('/api/health/status', healthRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`);
+});
