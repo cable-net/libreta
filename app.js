d72756b8c1978f24bd651fd11f147e7b91e369e0
betuzo <betotsol@gmail.com>
2022-07-11 12:02:41 -0700

    resolve conflicts

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const healthRoutes = require('./routes/health')
 
 app.use('/api/health/', healthRoutes)
 
-console.log('asdas')
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on: ${PORT}`)
